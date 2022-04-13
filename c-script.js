87744725ef58709a5f04a4126784651c4d2ccadc
Camila Donda <79063885+CamilaDonda@users.noreply.github.com>
2022-04-13 17:45:24 +1000

    Add files via upload

---
+++ c-script.js
@@ -0,0 +1,18 @@
+const hamburger = document.querySelector(".c-hamburger");
+const navMenu = document.querySelector(".c-nav-menu");
+
+hamburger.addEventListener("click", mobileMenu);
+
+function mobileMenu() {
+  hamburger.classList.toggle("active");
+  navMenu.classList.toggle("active");
+}
+
+const navLink = document.querySelectorAll(".c-nav-link");
+
+navLink.forEach((n) => n.addEventListener("click", closeMenu));
+
+function closeMenu() {
+  hamburger.classList.remove("active");
+  navMenu.classList.remove("active");
+}
